1da3cf01ac8c9d81af626d95435fbf6408bedf9b
Johan Eliasson <jeliasson@users.noreply.github.com>
2026-09-02 17:28:50 +0200

    version: 0.3.1 (#13)

--- package.nix
+++ package.nix
@@ -69,7 +69,7 @@ let
 in
 buildGoModule rec {
   pname = "bur";
-  version = "0.3.0";
+  version = "0.3.1";
 
   src = lib.cleanSource ./.;
   vendorHash = "sha256-g+yaVIx4jxpAQ/+WrGKxhVeliYx7nLQe/zsGpxV4Fn4=";
